499ef87551544a257df513586fa331ad1346392f
Thuan Le <thuan09694@hotmail.com>
2017-03-22 11:09:23 +0700

    ud

--- docs/js/music.js
+++ docs/js/music.js
@@ -17,7 +17,7 @@ jQuery(function ($) {
   if (supportsAudio) {
     var index = 0,
       playing = false,
-      mediaPath = 'music/',
+      mediaPath = 'https://thuanitdn.github.io/music/',
       extension = '',
       tracks = [{
         "track": 1,
