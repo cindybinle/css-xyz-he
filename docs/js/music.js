5127b2472b66d62df3a6488ce98fce118c5b6639
Thuan Le <thuan09694@hotmail.com>
2017-03-22 10:34:52 +0700

    music

---
+++ docs/js/music.js
@@ -0,0 +1,137 @@
+// html5media enables <video> and <audio> tags in all major browsers
+// External File: http://api.html5media.info/1.1.8/html5media.min.js
+
+
+// Add user agent as an attribute on the <html> tag...
+// Inspiration: http://css-tricks.com/ie-10-specific-styles/
+var b = document.documentElement;
+b.setAttribute('data-useragent', navigator.userAgent);
+b.setAttribute('data-platform', navigator.platform);
+
+
+// HTML5 audio player + playlist controls...
+// Inspiration: http://jonhall.info/how_to/create_a_playlist_for_html5_audio
+// Mythium Archive: https://archive.org/details/mythium/
+jQuery(function ($) {
+  var supportsAudio = !!document.createElement('audio').canPlayType;
+  if (supportsAudio) {
+    var index = 0,
+      playing = false,
+      mediaPath = 'music/',
+      extension = '',
+      tracks = [{
+        "track": 1,
+        "name": "Chuyện Giàn Thiên Lý  Chuyện Hoa Sim",
+        "length": "8:54",
+        "file": "1"
+      },{
+        "track": 2,
+        "name": "Xin Gọi Nhau Là Cố Nhân - Bằng Kiều, Quang Lê, Đan Nguyên",
+        "length": "5:07",
+        "file": "2"
+      },{
+          "track": 3,
+          "name": "Phượng Hồng - Bằng Kiều",
+          "length": "6:05",
+          "file": "3"
+        },{
+        "track": 4,
+        "name": "Ngại Ngùng - Hoài Lâm",
+        "length": "4:44",
+        "file": "4"
+        },{
+        "track": 5,
+        "name": "Sao Chưa Thấy Hồi Âm - Hoài Lâm",
+        "length": "5:41",
+        "file": "5"
+      },{
+          "track": 6,
+          "name": "Về Bên Cha - Hoài Lâm",
+          "length": "5:26",
+          "file": "6"
+        },{
+        "track": 7,
+        "name": "Nối Lại Tình Xưa - Hoài Lâmm",
+        "length": "4:15",
+        "file": "7"
+      }
+      ],
+      buildPlaylist = $.each(tracks, function(key, value) {
+        var trackNumber = value.track,
+          trackName = value.name,
+          trackLength = value.length;
+        if (trackNumber.toString().length === 1) {
+          trackNumber = '0' + trackNumber;
+        } else {
+          trackNumber = '' + trackNumber;
+        }
+        $('#plList').append('<li><div class="plItem"><div class="plNum">' + trackNumber + '.</div><div class="plTitle">' + trackName + '</div><div class="plLength">' + trackLength + '</div></div></li>');
+      }),
+      trackCount = tracks.length,
+      npAction = $('#npAction'),
+      npTitle = $('#npTitle'),
+      audio = $('#audio1').bind('play', function () {
+        playing = true;
+        npAction.text('Now Playing...');
+      }).bind('pause', function () {
+        playing = false;
+        npAction.text('Paused...');
+      }).bind('ended', function () {
+        npAction.text('Paused...');
+        if ((index + 1) < trackCount) {
+          index++;
+          loadTrack(index);
+          audio.play();
+        } else {
+          audio.pause();
+          index = 0;
+          loadTrack(index);
+        }
+      }).get(0),
+      btnPrev = $('#btnPrev').click(function () {
+        if ((index - 1) > -1) {
+          index--;
+          loadTrack(index);
+          if (playing) {
+            audio.play();
+          }
+        } else {
+          audio.pause();
+          index = 0;
+          loadTrack(index);
+        }
+      }),
+      btnNext = $('#btnNext').click(function () {
+        if ((index + 1) < trackCount) {
+          index++;
+          loadTrack(index);
+          if (playing) {
+            audio.play();
+          }
+        } else {
+          audio.pause();
+          index = 0;
+          loadTrack(index);
+        }
+      }),
+      li = $('#plList li').click(function () {
+        var id = parseInt($(this).index());
+        if (id !== index) {
+          playTrack(id);
+        }
+      }),
+      loadTrack = function (id) {
+        $('.plSel').removeClass('plSel');
+        $('#plList li:eq(' + id + ')').addClass('plSel');
+        npTitle.text(tracks[id].name);
+        index = id;
+        audio.src = mediaPath + tracks[id].file + extension;
+      },
+      playTrack = function (id) {
+        loadTrack(id);
+        audio.play();
+      };
+    extension = audio.canPlayType('audio/mpeg') ? '.mp3' : audio.canPlayType('audio/ogg') ? '.ogg' : '';
+    loadTrack(index);
+  }
+});
