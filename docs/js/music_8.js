e853748ccb7aa05d07bba986b213042fb81f4a83
Thuan Le <thuan09694@hotmail.com>
2017-07-07 17:44:38 +0900

    ud

--- docs/js/music_8.js
+++ docs/js/music_8.js
@@ -12,6 +12,83 @@ b.setAttribute('data-platform', navigator.platform);
 // HTML5 audio player + playlist controls...
 // Inspiration: http://jonhall.info/how_to/create_a_playlist_for_html5_audio
 // Mythium Archive: https://archive.org/details/mythium/
+
+var lyrics = [
+  {
+    "name": "Đắp Mộ Cuộc Tình - Lê Sang",
+    "length": "5:21",
+    "file": "15"
+  },
+  {
+    "name": "Gặp Nhau Làm Ngơ - Hoài Lâm",
+    "length": "4:05",
+    "file": "16"
+  },
+  {
+    "name": "Trước Ngày Hội Bắn - Anh Thơ, Việt Hoàn",
+    "length": "5:58",
+    "file": "14"
+  }, {
+    "name": "Xin Gọi Nhau Là Cố Nhân - Bằng Kiều, Quang Lê, Đan Nguyên",
+    "length": "5:07",
+    "file": "2"
+  }, {
+    "name": "Phượng Hồng - Bằng Kiều",
+    "length": "6:05",
+    "file": "3"
+  }, {
+    "name": "Ngại Ngùng - Hoài Lâm",
+    "length": "4:44",
+    "file": "4"
+  }, {
+    "name": "Sao Chưa Thấy Hồi Âm - Hoài Lâm",
+    "length": "5:41",
+    "file": "5"
+  }, {
+    "name": "Về Bên Cha - Hoài Lâm",
+    "length": "5:26",
+    "file": "6"
+  }, {
+    "name": "Nối Lại Tình Xưa - Hoài Lâm",
+    "length": "4:15",
+    "file": "7"
+  }, {
+    "name": "Phút đầu tiên - Nguyên Lê, Quỳnh Dung",
+    "length": "4:42",
+    "file": "8"
+  }, {
+    "name": "Sông Quê - Khánh Bình",
+    "length": "6:13",
+    "file": "9"
+  }, {
+    "name": "Sen Hồng Hư Không - Nam Du",
+    "length": "4:34",
+    "file": "10"
+  }, {
+    "name": "Thuyền Hoa - Hoài Lâm",
+    "length": "4:40",
+    "file": "11"
+  }, {
+    "name": "Phải Lòng Con Gái Bến Tre - Tố My",
+    "length": "5:56",
+    "file": "12"
+  }, {
+    "name": "Thói Đời - Hoài Lâm",
+    "length": "4:27",
+    "file": "13"
+  }, {
+    "name": "Chuyện Giàn Thiên Lý  Chuyện Hoa Sim",
+    "length": "8:54",
+    "file": "1"
+  }
+];
+
+lyrics.map(function (obj, index) {
+  obj.track = index + 1;
+});
+
+console.log(lyrics);
+
 jQuery(function ($) {
   var supportsAudio = !!document.createElement('audio').canPlayType;
   if (supportsAudio) {
@@ -19,86 +96,8 @@ jQuery(function ($) {
       playing = false,
       mediaPath = 'https://thuanitdn.github.io/music/',
       extension = '',
-      tracks = [
-        {
-          "track": 0,
-          "name": "Đắp Mộ Cuộc Tình - Lê Sang",
-          "length": "5:21",
-          "file": "15"
-        },
-        {
-          "track": 1,
-          "name": "Trước Ngày Hội Bắn - Anh Thơ, Việt Hoàn",
-          "length": "5:58",
-          "file": "14"
-        },{
-        "track": 2,
-        "name": "Xin Gọi Nhau Là Cố Nhân - Bằng Kiều, Quang Lê, Đan Nguyên",
-        "length": "5:07",
-        "file": "2"
-      },{
-          "track": 3,
-          "name": "Phượng Hồng - Bằng Kiều",
-          "length": "6:05",
-          "file": "3"
-        },{
-        "track": 4,
-        "name": "Ngại Ngùng - Hoài Lâm",
-        "length": "4:44",
-        "file": "4"
-        },{
-        "track": 5,
-        "name": "Sao Chưa Thấy Hồi Âm - Hoài Lâm",
-        "length": "5:41",
-        "file": "5"
-      },{
-          "track": 6,
-          "name": "Về Bên Cha - Hoài Lâm",
-          "length": "5:26",
-          "file": "6"
-        },{
-        "track": 7,
-        "name": "Nối Lại Tình Xưa - Hoài Lâm",
-        "length": "4:15",
-        "file": "7"
-      },{
-        "track": 8,
-        "name": "Phút đầu tiên - Nguyên Lê, Quỳnh Dung",
-        "length": "4:42",
-        "file": "8"
-      },{
-        "track": 9,
-        "name": "Sông Quê - Khánh Bình",
-        "length": "6:13",
-        "file": "9"
-      },{
-        "track": 10,
-        "name": "Sen Hồng Hư Không - Nam Du",
-        "length": "4:34",
-        "file": "10"
-      },{
-        "track": 11,
-        "name": "Thuyền Hoa - Hoài Lâm",
-        "length": "4:40",
-        "file": "11"
-      },{
-        "track": 12,
-        "name": "Phải Lòng Con Gái Bến Tre - Tố My",
-        "length": "5:56",
-        "file": "12"
-      },{
-          "track": 13,
-          "name": "Thói Đời - Hoài Lâm",
-          "length": "4:27",
-          "file": "13"
-        }, {
-          "track": 14,
-          "name": "Chuyện Giàn Thiên Lý  Chuyện Hoa Sim",
-          "length": "8:54",
-          "file": "1"
-        }
-      ],
-      buildPlaylist = $.each(tracks, function(key, value) {
+      tracks = lyrics,
+      buildPlaylist = $.each(tracks, function (key, value) {
         var trackNumber = value.track,
           trackName = value.name,
           trackLength = value.length;
